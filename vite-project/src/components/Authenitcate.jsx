08efb7912db7e1ac9b96e336b89747026f3116d0
Mokhtar Ouidah <mokhtarouidah@Mokhtars-iMac.local>
2023-08-21 21:14:29 -0400

    first commit

---
+++ vite-project/src/components/Authenitcate.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Authenticate() {
+  return (
+    <div>
+      <h2>Authenticate</h2>
+    </div>
+  );
+}
+
+export default Authenticate;
---
+++ vite-project/src/components/SignUpForm.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+
+function SignUpForm() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log("Form submission prevented. Async function called.");
+    // More form submission logic can be added here
+  };
+
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            Username:
+            <input
+              type="text"
+              value={username}
+              onChange={handleUsernameChange}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Password:
+            <input
+              type="password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+          </label>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default SignUpForm;
+
+
+
+
